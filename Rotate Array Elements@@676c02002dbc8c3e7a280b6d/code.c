d804263b2d2a64169b3ddf779ff55a083c301afa
Sanchit Mahajan <sm4939736@gmail.com>
2025-04-01 04:15:33 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,36 +1,21 @@
+// Your code here...
 #include<stdio.h>
-
 int main(){
     int N;
-    scanf("%d", &N);
-    
-    int arr[N]; // Dynamically sized array
-
-    // Read array elements
-    for(int i = 0; i < N; i++){
-        scanf("%d", &arr[i]);
+    int arr[sizeof(N)];
+    scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        scanf("%d",&arr[i]);
     }
-
     int k;
-    scanf("%d", &k);
-
-    // Swap the first k elements with the last k elements
-    for(int i = 0; i < k / 2; i++){
-        int temp = arr[i];
-        arr[i] = arr[k - i - 1];
-        arr[k - i - 1] = temp;
+    scanf("%d",&k);
+    for(int i=k-1,j=N-1;i>=0,j>=N-k;i--,j--){
+        int temp =arr[i];
+        arr[i]=arr[k];
+        arr[k]=temp;
+    }    
+    for(int i=0;i<N;i++){
+        printf("%d\n",arr[i]);
     }
 
-    for(int i = N - k; i < N; i++){
-        int temp = arr[i];
-        arr[i] = arr[N - i - 1];
-        arr[N - i - 1] = temp;
-    }
-    // Print the array after performing the operation
-    for(int i = 0; i < N; i++){
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    return 0;
 }
