6490c5ae6de5d7b3bf1781751faa405a454eb7ff
Sanchit Mahajan <sm4939736@gmail.com>
2025-04-01 05:17:35 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -12,7 +12,7 @@ int main() {
 
     int isSorted = 1;
 
-    for (int i = 1; i < N - 2; i++) {
+    for (int i = 0; i < N - 1; i++) {
         if (arr[i] > arr[i + 1]&&arr[i] > arr[i - 1]) {
             isSorted = arr[i];
             break;
