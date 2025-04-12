07fbedafcba8cf8a85822dfab348dc2b6d523c0e
Sanchit Mahajan <sm4939736@gmail.com>
2025-04-12 22:16:48 +0530

    Solution to Find the Longest Consecutive Sequence in an Array

--- Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
+++ Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
@@ -1,35 +1,48 @@
-// Your code here...
-#include<stdio.h>
-int longes(int a[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-1-i;j++){
-            if(a[j]>a[j+1]){
-                int t=a[j];
-                a[j]=a[j+1];
-                a[j+1]=t;
-            }
-        }
-    }
-    int curre=1;
-    int maxlen=1;
-    for(int i=1;i<n;i++){
-        if(a[i]==a[i-1]+1){
-            curre++;
-        }else{
-            if(curre>maxlen){
-                maxlen=curre;
-            }
-            curre=1;
-        }
-    }
-    printf("%d",maxlen);
-}
-int main(){
-    int n,a[n];
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
-    longes(a,n);
+#include <stdio.h>
 
+void longes(int a[], int n) {
+
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-1-i; j++) {
+            if (a[j] > a[j+1]) {
+                int t = a[j];
+                a[j] = a[j+1];
+                a[j+1] = t;
+            }
+        }
+    }
+
+    
+    int curre = 1;
+    int maxlen = 1;
+    for (int i = 1; i < n; i++) {
+        if (a[i] == a[i-1] + 1) {
+            curre++;
+        } else if (a[i] != a[i-1]) {
+            if (curre > maxlen) {
+                maxlen = curre;
+            }
+            curre = 1;
+        }
+    }
+    
+    
+    if (curre > maxlen) {
+        maxlen = curre;
+    }
+    
+    printf("%d", maxlen);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    longes(a, n);
+    return 0;
 }
